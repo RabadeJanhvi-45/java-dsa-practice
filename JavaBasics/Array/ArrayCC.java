2f85b14f6dade3e871b80ac39b04a16f3359fe16
RabadeJanhvi-45 <janhvirabade45@gmai.com>
2026-01-12 22:11:05 +0530

    Day5

--- JavaBasics/Array/ArrayCC.java
+++ JavaBasics/Array/ArrayCC.java
@@ -116,6 +116,22 @@ public class ArrayCC{
         }
          System.out.println("Total Subarray = "+ts);
     }
+    public static void maxSubayyay(int arr[]){
+        int ts=0;
+        for(int i=0;i<arr.length;i++){
+            int start=arr[i];
+            for(int j=i;j<arr.length;j++){
+                int end=j;
+                for(int k=start;k<=end;k++){
+                    System.out.print(arr[k]+ "  ");
+                }
+                ts++;
+                System.out.println();
+            }
+            System.out.println();
+        }
+         System.out.println("Total Subarray = "+ts);
+    }
     public static void main(String[] args){
     
     
